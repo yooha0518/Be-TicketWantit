ab8c3c0e68226ea8edeef0cec32a1af3bd8dc06f
Yoohayoung <81580399+yooha0518@users.noreply.github.com>
2023-04-18 18:18:58 +0900

    FEAT:user스키마생성

---
+++ models/schemas/types/short-id.js
@@ -0,0 +1,12 @@
+const { nanoid } = require('nanoid'); //고유한 문자열 생성 라이브러리
+
+const shortId = {
+	type: String,
+	default: () => {
+		return nanoid();
+	},
+	require: true,
+	index: true,
+};
+
+module.exports = shortId;
