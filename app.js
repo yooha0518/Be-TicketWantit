a8e5e28045914c6c3e5805493d182de81ee105e1
[SW4/1반] 정혜린 <flsgp0317@gmail.com>
2023-04-19 10:27:56 +0900

    [FEAT] 서버연결 및 스키마 추가

--- app.js
+++ app.js
@@ -1,49 +1,66 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
 const mongoose = require('mongoose');
-const env = require('./.env')
+const env = require('./.env');
 
-// const indexRouter = require('./routes');
-// const postsRouter = require('./routes/posts');
+//routers
+const indexRouter = require('./routers');
+const orderRouter = require('./routers/order');
+
+//서버연결
+app.listen(env.PORT, (err) => {
+	if(err){ 
+        console.log(`서벼 연결 실패 : ${err}`);
+    }else{
+        console.log(`서버 연결 성공`);
+    }
+})
 
 
+//db연결
+mongoose.connect(env.MONGO_URI);
 
-// 1. mongoose.connect()를 사용해서 mongodb 데이터베이스를 연결하세요.
-mongoose.connect(env.MONGO_URI)
-
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB Connected');
+mongoose.connection.on('connected', (err) => {
+    if(err){
+        console.log('MongoDB 연결중 에러 발생: ' + err );
+    }  
+    console.log('MongoDB Connected');
 });
+
+mongoose.connection.on('disconnected',(err)=>{
+    if(err){
+        console.log(`MongoDB 연결중 에러 발생: ` +err)
+    }
+    console.log('MongoDB disconnected')
+    console.log('byebye')
+}); 
 
 const app = express();
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/posts', postsRouter);
+//routes
+app.use('/', indexRouter);
+app.use('/orders', orderRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
