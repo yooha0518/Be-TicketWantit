1fbfd04bf4e06836602feb9aa1a24b9035f5cb88
[SW4/1반] 정혜린 <flsgp0317@gmail.com>
2023-04-25 19:36:23 +0900

    [FIX] merge conflict resolved

--- models/schemas/order.js
+++ models/schemas/order.js
@@ -4,22 +4,6 @@ const orderId = require('./types/short-id');
 const dayjs = require('dayjs');
 const formattedDate = dayjs().format('YYYY-MM-DD');
 
-<<<<<<< HEAD
-const orderSchema = new Schema({
-  orderId: {
-    type: String, // 스키마 유형을 String으로 지정합니다.
-    required: true, // 필요한 경우 다른 옵션도 추가합니다.
-    default: orderId,
-  },
-  customerId: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-  },
-  items: [
-    {
-      //상품
-      name: {
-=======
 const orderSchema = new Schema({ 
     orderId: {
         type: String, 
@@ -32,7 +16,6 @@ const orderSchema = new Schema({
     },
     items: [{ 
         name: {
->>>>>>> 2ec5c2df2c43bc97276c58ac51b470dd4f8500b9
         type: String,
         required: true,
       },
