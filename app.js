e0fbfef778c1aae59ab5483967b2a0c59d1fd2e9
[SW4/1반] 정혜린 <flsgp0317@gmail.com>
2023-04-25 22:59:09 +0900

    [FIX] 유저의 주문조회 내역 userId및 orderId 수정

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // app.use(logger('dev'));
 
-//app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(
 //    session({
--- services/orderService.js
+++ services/orderService.js
@@ -1,39 +1,46 @@
-const { Order } = require('../models');
-const { User } = require('../models');
-const dayjs = require('dayjs');
-const orderDate = dayjs().format('YYYYMMDD');
+const { Order } = require("../models");
+const { User } = require("../models");
 
 const orderService = {
   // 주문 추가 (주문하기)
   async createOrder({
-    shortId,
+    userId,
     customerPhoneNum,
     customerAddress,
     items,
     totalPrice,
-    zipCode
+    zipCode,
   }) {
-    const user = await User.findOne({ shortId });
-    if (!customerPhoneNum || !customerAddress || !items || !totalPrice ||!zipCode) {
-      throw new Error('정보를 모두 입력해주세요');
+    const user = await User.findOne({ userId });
+    if (
+      !customerPhoneNum ||
+      !customerAddress ||
+      !items ||
+      !totalPrice ||
+      !zipCode
+    ) {
+      throw new Error("정보를 모두 입력해주세요");
     }
     const createdOrder = await Order.create({
-      orderId: orderDate + shortId,
+      userId,
       customerId: user._id,
       items,
       customerAddress,
       customerPhoneNum,
       totalPrice,
-      zipCode
+      zipCode,
     });
     return createdOrder;
   },
   // 유저 주문 조회
-  async getOrder(shortId) {
+  async getOrder(userId) {
+    console.log(userId);
     try {
-      const userOrder = await Order.find({ shortId }).populate('customerId').exec();
+      const userOrder = await Order.find({ userId })
+        .populate("customerId")
+        .exec();
       if (!userOrder) {
-        throw new Error('주문 내역이 없습니다');
+        throw new Error("주문 내역이 없습니다");
       }
       console.log(userOrder);
       return userOrder;
@@ -46,15 +53,15 @@ const orderService = {
   async deleteOrder(shortId, orderId) {
     try {
       const order = await Order.find({ shortId, orderId }).populate(
-        'customerId'
+        "customerId"
       );
       const orderStatus = order[0].orderStatus;
       if (orderStatus == 1) {
         await Order.deleteOne({ orderId });
-        console.log('유저 주문이 취소되었습니다.');
+        console.log("유저 주문이 취소되었습니다.");
         return 1;
       } else {
-        console.log('이미 배송된 상품입니다.');
+        console.log("이미 배송된 상품입니다.");
         return 2;
       }
     } catch (error) {
@@ -68,9 +75,9 @@ const orderService = {
     putTargetCustomerPhoneNum
   ) {
     try {
-      const order = await Order.find({ orderId: putTargetOrderId }).populate(
-        'customerId'
-      ).exec();
+      const order = await Order.find({ orderId: putTargetOrderId })
+        .populate("customerId")
+        .exec();
       console.log(order);
       const orderStatus = order[0].orderStatus;
       if (orderStatus == 1) {
@@ -81,13 +88,13 @@ const orderService = {
             customerPhoneNum: putTargetCustomerPhoneNum,
           }
         );
-        console.log('유저의 주문정보가 수정되었습니다');
+        console.log("유저의 주문정보가 수정되었습니다");
         return 1;
       } else {
         return 2;
       }
     } catch (error) {
-      console.log('유저의 주문정보 수정에 실패했습니다.');
+      console.log("유저의 주문정보 수정에 실패했습니다.");
       next(error);
     }
   },
