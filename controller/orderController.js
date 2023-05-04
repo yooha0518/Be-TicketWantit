f29f2d160a4dcb37b27cedee724c768c7d5e17c7
[SW4/1반] 정혜린 <flsgp0317@gmail.com>
2023-05-05 00:02:28 +0900

    [FEAT] ORDER스키마에 productId 추가

--- controller/orderController.js
+++ controller/orderController.js
@@ -11,6 +11,7 @@ const orderController = {
         deliveryPhoneNum,
         zipCode,
         imgUrl,
+        productId,
       } = req.body;
       const order = await orderService.createOrder({
         userId,
@@ -20,6 +21,7 @@ const orderController = {
         items,
         totalPrice,
         zipCode,
+        productId,
       });
       console.log("Order created successfully: " + order);
       res.status(200).json({ message: "data saved", orderId: order.orderId });
