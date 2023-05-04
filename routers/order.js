967e213b97811ea75e79f338845efe62eeb60a83
Yoohayoung <518hayoung@naver.com>
2023-05-05 02:26:49 +0900

    [REFACTOR] 구매확정 추가

--- routers/order.js
+++ routers/order.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const orderRouter = Router();
 const {orderController} = require('../controller');
+const deliveryRouter = require('./deliveryRouter');
 
 //주문추가
 orderRouter.post('/', orderController.postOrder );
@@ -13,5 +14,7 @@ orderRouter.delete('/:orderId',orderController.deleteOrder);
 
 //유저 주문 수정 - 배송 전까지 정보 수정(배송지, 휴대폰 번호)
 orderRouter.put('/:orderId',orderController.updateOrder);
+
+orderRouter.use('/delivery',deliveryRouter);
 
 module.exports = orderRouter;
