dfa4fb797ca75968da07139be698d45f822848b0
Yoohayoung <518hayoung@naver.com>
2023-05-06 03:37:10 +0900

    [REFACTOR] 서버 주소 수정

--- routers/postProfileImage.js
+++ routers/postProfileImage.js
@@ -5,7 +5,7 @@ const { User } = require('../models/index');
 postProfileImageRouter.post('/', async (req, res) => {
 	console.log('프로필사진 수정 시작');
 	const { shortId } = req.user;
-	const profileImage = `34.64.112.166/${req.file.filename}`;
+	const profileImage = `34.64.112.166:5000/${req.file.filename}`;
 	const result = await User.updateOne(
 		{ shortId },
 		{
