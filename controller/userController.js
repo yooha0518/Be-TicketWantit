74091c8fa1eae69b40fcf106b5851eeffe818f03
Yoohayoung <518hayoung@naver.com>
2023-05-07 22:17:20 +0900

    [REFACTOR] 이미지 경로 수정

--- controller/userController.js
+++ controller/userController.js
@@ -90,7 +90,7 @@ const userController = {
 		try {
 			console.log('프로필사진 수정 시작');
 			const { shortId } = req.user;
-			const profileImage = `34.64.112.166:5000/${req.file.filename}`;
+			const profileImage = `https://34.64.112.166:5000/${req.file.filename}`;
 			const result = await userService.updateProfileImage(
 				shortId,
 				profileImage
@@ -228,6 +228,7 @@ const userController = {
 				address,
 				zipCode,
 				phoneNumber,
+				profileImage,
 			});
 			res.status(200).json(result);
 		} catch (error) {
