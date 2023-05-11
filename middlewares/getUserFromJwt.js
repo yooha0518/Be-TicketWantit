5447d36f9ea4bd983ea3308a51fce0ca4e33d3e6
Yoohayoung <518hayoung@naver.com>
2023-05-11 23:02:00 +0900

    [REFACTOR]관리자- 사용자 검색기능 추가,필요없는 log 삭제

--- middlewares/getUserFromJwt.js
+++ middlewares/getUserFromJwt.js
@@ -1,13 +1,10 @@
 const passport = require('passport');
 
 module.exports = (req, res, next) => {
-	console.log('getUserFromJwt 미들웨어 실행');
-
-	console.log('인증전략을 사용하여 검사 시작');
 	//토큰검증 미들웨어
 	passport.authenticate('access', { session: false })(req, res, (err) => {
 		if (err) {
-			console.log('authenticate 에러남');
+			console.log('authenticate 에러');
 			res.status(500).send(err.message);
 		} else {
 			console.log('Authorized');
